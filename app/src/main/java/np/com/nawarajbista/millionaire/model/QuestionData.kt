d96378993b5110a7b6345fd979684b03cdf80273
nawadeveloper <nawarajusa@gmail.com>
2019-08-13 01:29:58 -0400

    data added

--- app/src/main/java/np/com/nawarajbista/millionaire/model/QuestionData.kt
+++ app/src/main/java/np/com/nawarajbista/millionaire/model/QuestionData.kt
@@ -4,42 +4,225 @@ open class QuestionData {
 
     private val thousandDollar = arrayOf(
         mapOf(
-            "q" to "What is your name?",
-            "optionCorrect" to "Nawaraj",
-            "option2" to "Ranju",
-            "option3" to "Nikesh",
-            "option4" to "Jayenta"
+            "q" to "On a radio, stations are changed by using what control?",
+            "optionCorrect" to "Tuning",
+            "option2" to "Volume",
+            "option3" to "Bass",
+            "option4" to "Treble"
         ),
         mapOf(
-            "q" to "What is your age?",
-            "optionCorrect" to "31",
-            "option2" to "30",
-            "option3" to "35",
-            "option4" to "28"
+            "q" to "Which state in the United States is largest by area?",
+            "optionCorrect" to "Alaska",
+            "option2" to "California",
+            "option3" to "Texas",
+            "option4" to "Hawaii"
+        ),
+        mapOf(
+            "q" to "What is Aurora Borealis commonly known as?",
+            "optionCorrect" to "Northern Lights",
+            "option2" to "Fairy Dust",
+            "option3" to "Book of ages",
+            "option4" to "a Game of Thrones main character"
+        ),
+        mapOf(
+            "q" to "Galileo was an Italian astronomer who",
+            "optionCorrect" to "All of the above",
+            "option2" to "discovered four satellites of Jupiter",
+            "option3" to "discovered that the movement of pendulum produces a regular time measurement",
+            "option4" to "developed the telescope"
+
+        ),
+        mapOf(
+            "q" to "Exposure to sunlight helps a person improve his health because",
+            "optionCorrect" to "the ultraviolet rays convert skin oil into Vitamin D",
+            "option2" to "the infrared light kills bacteria in the body",
+            "option3" to "resistance power increases",
+            "option4" to "the pigment cells in the skin get stimulated and produce a healthy tan"
         )
     )
 
-    private val millionDollar = arrayOf(
+
+
+
+
+    private val tenThousandDollar = arrayOf(
         mapOf(
-            "q" to "What are your doing?",
-            "optionCorrect" to "working",
-            "option2" to "studying",
-            "option3" to "sleeping",
-            "option4" to "driving"
+            "q" to "Oscar Awards were instituted in",
+            "optionCorrect" to "1929",
+            "option2" to "1968",
+            "option3" to "1901",
+            "option4" to "1965"
+        ),
+
+        mapOf(
+            "q" to "When is the International Workers' Day?",
+            "optionCorrect" to "1st May",
+            "option2" to "15th April",
+            "option3" to "12th December",
+            "option4" to "1st August"
         ),
         mapOf(
-            "q" to "When you come to USA?",
-            "optionCorrect" to "2016",
+            "q" to "In the U.S., if it's not Daylight Saving Time, it's what?",
+            "optionCorrect" to "Standard time",
+            "option2" to "Borrowed time",
+            "option3" to "Overtime",
+            "option4" to "Party time"
+        ),
+        mapOf(
+            "q" to "Which country is largest by area?",
+            "optionCorrect" to "Russia",
+            "option2" to "UK",
+            "option3" to "USA",
+            "option4" to "China"
+        ),
+        mapOf(
+            "q" to "Light Year is related to",
+            "optionCorrect" to "distance",
+            "option2" to "energy",
+            "option3" to "time",
+            "option4" to "speed"
+        )
+    )
+
+
+
+
+
+
+
+
+
+    private val twentyThousandDollar = arrayOf(
+        mapOf(
+            "q" to "\"When did China test their first atomic device?\"",
+            "optionCorrect" to "1964",
+            "option2" to "1969",
+            "option3" to "1950",
+            "option4" to "1961"
+        ),
+
+        mapOf(
+            "q" to "When did Margaret Thatcher became the first female Prime Minister of Britain?",
+            "optionCorrect" to "1979",
+            "option2" to "1998",
+            "option3" to "1989",
+            "option4" to "1800"
+        ),
+        mapOf(
+            "q" to "What was the first university in the United States",
+            "optionCorrect" to "Harvard",
+            "option2" to "University of Washington",
+            "option3" to "Yale",
+            "option4" to "Oxford"
+        ),
+        mapOf(
+            "q" to "What is the S.I. unit of temperature?",
+            "optionCorrect" to "Kelvin",
+            "option2" to "Celsius",
+            "option3" to "Centigrade",
+            "option4" to "Fahrenheit"
+        ),
+        mapOf(
+            "q" to "Which of these African countries is situated south of the equator?",
+            "optionCorrect" to "Zambia",
+            "option2" to "Ethiopia",
+            "option3" to "Nigeria",
+            "option4" to "Chad"
+
+        )
+    )
+
+
+
+
+
+    private val fiftyThousandDollar = arrayOf(
+        mapOf(
+            "q" to "When did US astronauts Neil Armstrong and Edwin E. Aldrin land on the moon?",
+            "optionCorrect" to "July 21, 1969",
+            "option2" to "July 21, 1970",
+            "option3" to "July 21, 1963",
+            "option4" to "July 21, 1972"
+        ),
+
+        mapOf(
+            "q" to "",
+            "optionCorrect" to "",
+            "option2" to "",
+            "option3" to "",
+            "option4" to ""
+        ),
+        mapOf(
+            "q" to "When is the World's Diabetes Day?",
+            "optionCorrect" to "14th November",
+            "option2" to "11th December",
+            "option3" to "15th October",
+            "option4" to "1st July"
+        ),
+        mapOf(
+            "q" to "What is the second most common gas in the air?",
+            "optionCorrect" to "Oxygen",
+            "option2" to "Nitrogen",
+            "option3" to "Water",
+            "option4" to "Helium"
+        ),
+        mapOf(
+            "q" to "The American General who led the revolt against the British and declared American independence was",
+            "optionCorrect" to "George Washington",
+            "option2" to "Bill Clinton",
+            "option3" to "George Bush",
+            "option4" to "None of the above"
+        )
+    )
+
+
+
+
+
+    private val millionDollar = arrayOf(
+        mapOf(
+            "q" to "What is the name of the largest freshwater lake in the world?",
+            "optionCorrect" to "Lake Union",
+            "option2" to "Lake Superior",
+            "option3" to "Lake Largest",
+            "option4" to "Lake Goodwin"
+        ),
+        mapOf(
+            "q" to "Hamid Karzai was chosen president of Afghanistan in",
+            "optionCorrect" to "2002",
             "option2" to "2000",
-            "option3" to "2015",
-            "option4" to "2008"
+            "option3" to "2001",
+            "option4" to "2003"
+        ),
+        mapOf(
+            "q" to "When and where was hockey introduced for women in Olympics?",
+            "optionCorrect" to "1980 at Moscow",
+            "option2" to "1908 at London",
+            "option3" to "1936 at Berlin",
+            "option4" to "1924 at Paris"
+        ),
+        mapOf(
+            "q" to "When did France became Republic?",
+            "optionCorrect" to "1792 AD",
+            "option2" to "1789 AD",
+            "option3" to "1798 AD",
+            "option4" to "1729 AD"
+        ),
+        mapOf(
+            "q" to "When and where was weightlifting introduced in Olympics?",
+            "optionCorrect" to "1986 at Athens",
+            "option2" to "1988 at Seoul",
+            "option3" to "1924 at St. Louis",
+            "option4" to "1908 at London"
         )
     )
 
     val data = arrayOf(
         thousandDollar,
+        tenThousandDollar,
+        twentyThousandDollar,
+        fiftyThousandDollar,
         millionDollar
     )
-
 
 }
